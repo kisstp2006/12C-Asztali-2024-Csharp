a3ce1d51c302cfde34815630823a52b906f86597
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2024-11-06 08:51:36 +0100

    Kor 2 implemented

--- Kör hasáb/Kor2.cs
+++ Kör hasáb/Kor2.cs
@@ -8,5 +8,38 @@ namespace Kör_hasáb
 {
     class Kor2
     {
+        //Osztaly változó
+        private double sugar, kerulet, terulet;
+
+        public Kor2()
+        {
+            
+        }
+        public Kor2(double r)
+        {
+            this.sugar = r;
+            Szamitások();
+
+        }
+        public void SetSugar(double r)
+        {
+            this.sugar = r;
+            Szamitások();
+
+        }
+        private void CalcKerulet()
+        {
+           this.kerulet= 2* this.sugar * Math.PI;
+        }
+        private double Calcterulet(double r)
+        {
+
+            return Math.Pow(this.sugar, 2) * Math.PI; ;
+        }
+        private void Szamitások()
+        {
+            CalcKerulet();
+            this.terulet = Calcterulet(this.sugar);
+        }
     }
 }
