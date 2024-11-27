99f212b7488a8c6f1416da4bf4badfd07d800752
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2024-11-27 08:37:04 +0100

    added auto test and manual test

--- Háromszögek típusa/Program.cs
+++ Háromszögek típusa/Program.cs
@@ -1,23 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Háromszögek_típusa
 {
     class Program
     {
-        public int  a, b, c;
         static void Main(string[] args)
         {
-            Console.WriteLine("Kérem az A oldalt:");
-            a = int.Parse(Console.ReadLine());
-            HTipus haromszogtipus = new HTipus(1,2,5);
-            if (haromszogtipus.EzHaromszog())
-            {
+            // Automatikus tesztelés véletlenszerű értékekkel
+            Random rand = new Random();
+            int a = rand.Next(1, 20);
+            int b = rand.Next(1, 20);
+            int c = rand.Next(1, 20);
 
-            }
+            HTipus randomTriangle = new HTipus(a, b, c);
+            Console.WriteLine($"Random háromszög oldalak: a={a}, b={b}, c={c}");
+            PrintTriangleProperties(randomTriangle);
+
+            // Felhasználói bemenet
+            Console.WriteLine("\nKérem az oldalakat:");
+            Console.Write("a: ");
+            int userA = int.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            int userB = int.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            int userC = int.Parse(Console.ReadLine());
+
+            HTipus userTriangle = new HTipus(userA, userB, userC);
+            PrintTriangleProperties(userTriangle);
+        }
+
+        static void PrintTriangleProperties(HTipus triangle)
+        {
+            Console.WriteLine($"Ez háromszög: {triangle.EzHaromszog()}");
+            Console.WriteLine($"Derékszögű: {triangle.Derekszogue()}");
+            Console.WriteLine($"Egyenlő szárú: {triangle.Egyenloszarue()}");
+            Console.WriteLine($"Egyenlő oldalú: {triangle.Egyenlooldalu()}");
         }
     }
 }
