7a8b309a3103f207241c09c2ad5eefe1ad3efcb0
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2024-11-27 10:05:42 +0100

    Added error cathing

---
+++ Kivetel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kivetelek kivet = new kivetelek();
+            kivet.Nincskivetel();
+            kivet.Altalanoshibakezeles();
+        }
+    }
+}
---
+++ Kivetel/kivetelek.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class kivetelek
+    {
+        public void Nincskivetel()
+        {
+            Console.WriteLine("Nincs kivetel kezeles");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A beírt szám:{szam}");
+            Console.ReadKey();
+        }
+
+        public void Altalanoshibakezeles()
+        {
+            Console.WriteLine("Van kivetel kezeles");
+
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"A beírt szám:{szam}");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("hiba");
+                //throw;
+            }
+
+        }
+        public void Altalanoshibakezeles2()
+        {
+            Console.WriteLine("Van kivetel kezeles");
+
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"A beírt szám:{szam}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("A következő hiba lépett fel :", e);
+                //throw;
+            }
+
+        }
+
+        public void Hibakezeles2()
+        {
+
+        }
+    }
+}
