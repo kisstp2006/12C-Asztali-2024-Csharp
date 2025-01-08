3444bcc011273ae88ac2b6122ebda7c228926f69
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-01-08 09:33:53 +0100

    Implemented JarmuClass

---
+++ AutoList/JarmuClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoList
+{
+    class JarmuClass
+    {
+        private string Rendszam { get; set; }
+        private string Gyartmany { get; set; }
+        private string Típus { get; set; }
+        private int Gyartasiév { get; set; }
+        private DateTime Muszakivizsgaervenyessege { get; set; }
+        private string TulajNeve { get; set; }
+
+
+        public JarmuClass(string rendszam, string gyartmany, string tipus, int gyartasiEv, DateTime muszakiVizsgaErvenyesseg, string tulajdonosNeve)
+        {
+            Rendszam = rendszam;
+            Gyartmany = gyartmany;
+            Típus = tipus;
+            Gyartasiév = gyartasiEv;
+            Muszakivizsgaervenyessege = muszakiVizsgaErvenyesseg;
+            TulajNeve = tulajdonosNeve;
+        }
+        public override string ToString()
+        {
+            return  $"{Rendszam,-10} | {Gyartmany,-10} | {Típus,-10} | {Gyartasiév,-4} | {Muszakivizsgaervenyessege.ToShortDateString(),-15} | {TulajNeve}";
+
+        }
+    }
+}
--- AutoList/Program.cs
+++ AutoList/Program.cs
@@ -8,9 +8,41 @@ namespace AutoList
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            // A jarmuvek listaja
+            List<JarmuClass> jarmuvek = new List<JarmuClass>();
 
+            // Adja meg a file nevét
+            Console.Write("Kérem adja meg a fájl nevét: ");
+            string fileName = Console.ReadLine();
+            //Kiserlet a file beolvasására
+
+            try
+            {
+                Beolvas(fileName, jarmuvek);
+                Console.WriteLine("\nAdatok sikeresen beolvasva.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a fájl beolvasása során: " + ex.Message);
+                return;
+            }
+            // Kiíratás táblázatos formában
+            Console.WriteLine("\nA beolvasott adatok:");
+            Kiir(jarmuvek);
+        }
+
+        private static void Kiir(List<JarmuClass> jarmuvek)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Beolvas(string fileName, List<JarmuClass> jarmuvek)
+        {
+
+            throw new NotImplementedException();
         }
     }
 }
