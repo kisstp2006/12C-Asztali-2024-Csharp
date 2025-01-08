7c131666c303d4bc9f5cdc3eb1c5cbdc53aab7bf
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-01-08 09:56:01 +0100

    Updated

---
+++ BaratList/BaratClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratList { 
+
+    public class BaratClass
+    {
+        public string Nev { get; set; }
+        public DateTime SzuletesiDatum { get; set; }
+        public string Nem { get; set; }
+        public int BulizasiHajlam { get; set; }
+
+        // Konstruktor
+        public BaratClass(string nev, DateTime szuletesiDatum, string nem, int bulizasiHajlam)
+        {
+            Nev = nev;
+            SzuletesiDatum = szuletesiDatum;
+            Nem = nem;
+            BulizasiHajlam = bulizasiHajlam;
+        }
+    }
+
+}
