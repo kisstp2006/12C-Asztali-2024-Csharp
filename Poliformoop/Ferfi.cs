8112cc50c83f6da6c4b530300be1e6000ab494a4
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-01-15 08:41:18 +0100

    Finished Woman and Man

---
+++ Poliformoop/Ferfi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliformoop
+{
+    class Ferfi : Nber
+    {
+        public int Kor { get; set; }
+         
+        public Ferfi(string nev, int kor):
+            base(nev)
+            {
+            Kor = kor;
+
+        }
+    }
+}
---
+++ Poliformoop/No.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliformoop
+{
+    class No : Nber
+    {
+        public string SzemSzin { get; set; }
+        public No (string nev ,string szemszing):
+            base(nev)
+        {
+            SzemSzin = szemszing;
+        }
+    }
+}
