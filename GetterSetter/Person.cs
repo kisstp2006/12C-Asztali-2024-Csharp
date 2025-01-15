c13d279bc9968f7f1044efc5363b296b7d2ac07a
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-01-15 09:48:31 +0100

    Added Person program and code with updated poliform oop

---
+++ GetterSetter/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterSetter
+{
+    class Person
+    {
+        public Person() { }
+
+        public string Name { get; set; }
+        private int id;
+        public int ID { get => id; set=> id=101 ; }
+
+        private int age;
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+        public int getAge() { return this.age; }
+        private int gender;
+        public void SetGender(string gender)
+        {
+            switch (gender)
+            {
+                case "Férfi" :
+                    this.gender = 1;
+                    break;
+                case "Nő":
+                    this.gender = 2;
+                    break;
+                default:
+                    this.gender = 0;
+                    break;
+            }
+        }
+    }
+}
--- Poliformoop/PoliformOOP.cs
+++ Poliformoop/PoliformOOP.cs
@@ -10,7 +10,39 @@ namespace Poliformoop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ASD");
+
+            Haz Otthon = new Haz();
+
+            No eva = new No("Eva", "zold");
+            No lara = new No("Lara", "barna");
+            No kati = new No("Kati", "barna");
+            No sari = new No("Sari", "kék");
+            No zsuzsi = new No("Zsuzsi", "zöldd");
+            Ferfi ferfi = new Ferfi("Pisti",40);
+            Ferfi adam = new Ferfi("Ádám",20);
+            Ferfi dani = new Ferfi("Dani",20);
+            Ferfi gergp = new Ferfi("Gergp",20);
+            Ferfi attila = new Ferfi("Attola",20);
+
+            if (eva is Nber) Otthon.hazajon((Nber)eva);
+            Otthon.hazajon((Nber)eva);
+            Otthon.hazajon((Nber)lara);
+            Otthon.hazajon((Nber)kati);
+            Otthon.hazajon((Nber)sari);
+            Otthon.hazajon((Nber)zsuzsi);
+            Otthon.hazajon((Nber)ferfi);
+            Otthon.hazajon((Nber)adam);
+            Otthon.hazajon((Nber)gergp);
+            Otthon.hazajon((Nber)dani);
+            Otthon.hazajon((Nber)attila);
+
+            bool tovabb = true;
+            do
+            {
+                Otthon.csenget();
+                tovabb = (Console.ReadLine() == String.Empty) ? false : true;
+            } while (tovabb);
+
             Console.ReadKey();
         }
     }
