8aea60bfbe0680d58814663562cfa201f9eccb78
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-02-05 09:42:53 +0100

    Abstract 1

---
+++ Abstract/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+            /*Ez a példa a Abstract kulcsszó használatát mutatja be 
+             *Az Abstract kulcsszóval olyan osztályokat és metódusokat jelölünk amelyeket nem lehet közvetlenül pédányosítani 
+             *illetve végrehalytani . Az Abstract osztályokat alapként hasnálják més osztályok számára,
+             *míg az Abstract metódusok deklarációkat tartalmaznak, de nem admak meg implementációt
+             */
+
+            /* Fontos jellenzők
+             * -Az Abstract osztály csak leszármazott osztályokon keresztül használható. Tartalmazhat Abstract és nem Abstract (Működő)osztályokat is
+             * -Abstract metódus: Csak Abstract osztályban lehet származtatott osztálynak kötelező megvalósítani.
+             */
+namespace Abstract
+{
+    abstract class Shape
+    {
+        //Abstract metódus - nincs implementáció
+        //Ennek a kódjárt a leszármazottban kötelező megírni
+
+        public abstract double GetArea();
+
+        //Normál metódus - van implementáció
+
+        public void DisplayShape()
+        {
+            Console.WriteLine("Ez egy alakzat");
+        }
+
+    }
+}
