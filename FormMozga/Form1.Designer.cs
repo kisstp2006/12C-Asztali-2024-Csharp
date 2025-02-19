ec36c034be970d7386d1b2754a3c12de2f12d636
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-02-19 10:04:04 +0100

    Update 1

---
+++ FormMozga/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace FormMozga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnJobb = new Button();
+            btnFel = new Button();
+            btnFelul = new Button();
+            btnJobbszel = new Button();
+            btnLe = new Button();
+            btnAlul = new Button();
+            button9 = new Button();
+            btnBalra = new Button();
+            btnBalszel = new Button();
+            gpbAtlatszo = new GroupBox();
+            btnOpCsok = new Button();
+            btnOpNo = new Button();
+            label2 = new Label();
+            gpbMeret = new GroupBox();
+            btnCsok = new Button();
+            btnMeretNo = new Button();
+            label1 = new Label();
+            gpbAtlatszo.SuspendLayout();
+            gpbMeret.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnJobb
+            // 
+            btnJobb.Location = new Point(458, 243);
+            btnJobb.Margin = new Padding(4, 3, 4, 3);
+            btnJobb.Name = "btnJobb";
+            btnJobb.Size = new Size(96, 33);
+            btnJobb.TabIndex = 0;
+            btnJobb.Text = "Jobbra";
+            btnJobb.UseVisualStyleBackColor = true;
+            // 
+            // btnFel
+            // 
+            btnFel.Location = new Point(349, 188);
+            btnFel.Margin = new Padding(4, 3, 4, 3);
+            btnFel.Name = "btnFel";
+            btnFel.Size = new Size(96, 33);
+            btnFel.TabIndex = 0;
+            btnFel.Text = "Fel";
+            btnFel.UseVisualStyleBackColor = true;
+            btnFel.Click += btnFel_Click;
+            // 
+            // btnFelul
+            // 
+            btnFelul.Location = new Point(349, 127);
+            btnFelul.Margin = new Padding(4, 3, 4, 3);
+            btnFelul.Name = "btnFelul";
+            btnFelul.Size = new Size(96, 33);
+            btnFelul.TabIndex = 0;
+            btnFelul.Text = "Felülre";
+            btnFelul.UseVisualStyleBackColor = true;
+            btnFelul.Click += btnFelul_Click;
+            // 
+            // btnJobbszel
+            // 
+            btnJobbszel.Location = new Point(568, 243);
+            btnJobbszel.Margin = new Padding(4, 3, 4, 3);
+            btnJobbszel.Name = "btnJobbszel";
+            btnJobbszel.Size = new Size(96, 33);
+            btnJobbszel.TabIndex = 0;
+            btnJobbszel.Text = "Jobbszélre";
+            btnJobbszel.UseVisualStyleBackColor = true;
+            // 
+            // btnLe
+            // 
+            btnLe.Location = new Point(349, 310);
+            btnLe.Margin = new Padding(4, 3, 4, 3);
+            btnLe.Name = "btnLe";
+            btnLe.Size = new Size(96, 33);
+            btnLe.TabIndex = 0;
+            btnLe.Text = "Lefele";
+            btnLe.UseVisualStyleBackColor = true;
+            // 
+            // btnAlul
+            // 
+            btnAlul.Location = new Point(349, 372);
+            btnAlul.Margin = new Padding(4, 3, 4, 3);
+            btnAlul.Name = "btnAlul";
+            btnAlul.Size = new Size(96, 33);
+            btnAlul.TabIndex = 0;
+            btnAlul.Text = "Alulra";
+            btnAlul.UseVisualStyleBackColor = true;
+            btnAlul.Click += btnAlul_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(349, 243);
+            button9.Margin = new Padding(4, 3, 4, 3);
+            button9.Name = "button9";
+            button9.Size = new Size(96, 33);
+            button9.TabIndex = 0;
+            button9.Text = "Középre";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // btnBalra
+            // 
+            btnBalra.Location = new Point(239, 243);
+            btnBalra.Margin = new Padding(4, 3, 4, 3);
+            btnBalra.Name = "btnBalra";
+            btnBalra.Size = new Size(96, 33);
+            btnBalra.TabIndex = 0;
+            btnBalra.Text = "Balra";
+            btnBalra.UseVisualStyleBackColor = true;
+            // 
+            // btnBalszel
+            // 
+            btnBalszel.Location = new Point(130, 243);
+            btnBalszel.Margin = new Padding(4, 3, 4, 3);
+            btnBalszel.Name = "btnBalszel";
+            btnBalszel.Size = new Size(96, 33);
+            btnBalszel.TabIndex = 0;
+            btnBalszel.Text = "Balszélre";
+            btnBalszel.UseVisualStyleBackColor = true;
+            // 
+            // gpbAtlatszo
+            // 
+            gpbAtlatszo.BackColor = Color.FromArgb(255, 128, 0);
+            gpbAtlatszo.Controls.Add(btnOpCsok);
+            gpbAtlatszo.Controls.Add(btnOpNo);
+            gpbAtlatszo.Location = new Point(198, 440);
+            gpbAtlatszo.Margin = new Padding(4, 3, 4, 3);
+            gpbAtlatszo.Name = "gpbAtlatszo";
+            gpbAtlatszo.Padding = new Padding(4, 3, 4, 3);
+            gpbAtlatszo.Size = new Size(444, 66);
+            gpbAtlatszo.TabIndex = 1;
+            gpbAtlatszo.TabStop = false;
+            // 
+            // btnOpCsok
+            // 
+            btnOpCsok.Location = new Point(260, 22);
+            btnOpCsok.Margin = new Padding(4, 3, 4, 3);
+            btnOpCsok.Name = "btnOpCsok";
+            btnOpCsok.Size = new Size(96, 33);
+            btnOpCsok.TabIndex = 0;
+            btnOpCsok.Text = "Csökken";
+            btnOpCsok.UseVisualStyleBackColor = true;
+            btnOpCsok.Click += btnOpCsok_Click;
+            // 
+            // btnOpNo
+            // 
+            btnOpNo.Location = new Point(150, 22);
+            btnOpNo.Margin = new Padding(4, 3, 4, 3);
+            btnOpNo.Name = "btnOpNo";
+            btnOpNo.Size = new Size(96, 33);
+            btnOpNo.TabIndex = 0;
+            btnOpNo.Text = "Növekszik";
+            btnOpNo.UseVisualStyleBackColor = true;
+            btnOpNo.Click += btnOpNo_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label2.ForeColor = SystemColors.ButtonFace;
+            label2.Location = new Point(85, 459);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Átlátszóság: ";
+            // 
+            // gpbMeret
+            // 
+            gpbMeret.BackColor = Color.FromArgb(255, 128, 0);
+            gpbMeret.Controls.Add(btnCsok);
+            gpbMeret.Controls.Add(btnMeretNo);
+            gpbMeret.Location = new Point(198, 14);
+            gpbMeret.Margin = new Padding(4, 3, 4, 3);
+            gpbMeret.Name = "gpbMeret";
+            gpbMeret.Padding = new Padding(4, 3, 4, 3);
+            gpbMeret.Size = new Size(444, 66);
+            gpbMeret.TabIndex = 1;
+            gpbMeret.TabStop = false;
+            // 
+            // btnCsok
+            // 
+            btnCsok.Location = new Point(260, 18);
+            btnCsok.Margin = new Padding(4, 3, 4, 3);
+            btnCsok.Name = "btnCsok";
+            btnCsok.Size = new Size(96, 33);
+            btnCsok.TabIndex = 0;
+            btnCsok.Text = "Csökken";
+            btnCsok.UseVisualStyleBackColor = true;
+            btnCsok.Click += btnCsok_Click;
+            // 
+            // btnMeretNo
+            // 
+            btnMeretNo.Location = new Point(150, 18);
+            btnMeretNo.Margin = new Padding(4, 3, 4, 3);
+            btnMeretNo.Name = "btnMeretNo";
+            btnMeretNo.Size = new Size(96, 33);
+            btnMeretNo.TabIndex = 0;
+            btnMeretNo.Text = "Növekszik";
+            btnMeretNo.UseVisualStyleBackColor = true;
+            btnMeretNo.Click += btnMeretNo_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(144, 37);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Méret";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(819, 519);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(gpbMeret);
+            Controls.Add(gpbAtlatszo);
+            Controls.Add(btnBalszel);
+            Controls.Add(btnBalra);
+            Controls.Add(button9);
+            Controls.Add(btnAlul);
+            Controls.Add(btnLe);
+            Controls.Add(btnJobbszel);
+            Controls.Add(btnFelul);
+            Controls.Add(btnFel);
+            Controls.Add(btnJobb);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            gpbAtlatszo.ResumeLayout(false);
+            gpbMeret.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJobb;
+        private System.Windows.Forms.Button btnFel;
+        private System.Windows.Forms.Button btnFelul;
+        private System.Windows.Forms.Button btnJobbszel;
+        private System.Windows.Forms.Button btnLe;
+        private System.Windows.Forms.Button btnAlul;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnBalra;
+        private System.Windows.Forms.Button btnBalszel;
+        private System.Windows.Forms.GroupBox gpbAtlatszo;
+        private System.Windows.Forms.Button btnOpCsok;
+        private System.Windows.Forms.Button btnOpNo;
+        private System.Windows.Forms.GroupBox gpbMeret;
+        private System.Windows.Forms.Button btnCsok;
+        private System.Windows.Forms.Button btnMeretNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ FormMozga/Form1.cs
@@ -0,0 +1,86 @@
+namespace FormMozga
+{
+    public partial class Form1 : Form
+    {
+        //osztály változók
+        private int valtmagassag = 20, valtszelesseg = 20, windowsizewidth, windowsizeheight;
+        private float OpacityVar = 1f;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMeretNo_Click(object sender, EventArgs e)
+        {
+            windowsizewidth = Screen.GetWorkingArea(this).Width;
+            windowsizeheight = Screen.GetWorkingArea(this).Height;
+
+            if (windowsizeheight > Height + valtmagassag)
+                Height += valtmagassag;
+
+
+            if (windowsizewidth > Width + valtszelesseg)
+                Width += valtszelesseg;
+
+        }
+
+        private void btnCsok_Click(object sender, EventArgs e)
+        {
+            windowsizewidth = Screen.GetWorkingArea(this).Width;
+            windowsizeheight = Screen.GetWorkingArea(this).Height;
+            Console.WriteLine(Height);
+            Console.WriteLine(windowsizeheight / 2);
+
+            if (windowsizeheight / 2 < Height - valtmagassag)
+                Height -= valtmagassag;
+            if (windowsizewidth / 2 < Width - valtszelesseg)
+                Width -= valtszelesseg;
+        }
+
+        private void btnOpNo_Click(object sender, EventArgs e)
+        {
+
+            if (OpacityVar > 0.1)
+            {
+                OpacityVar -= 0.1f;
+            }
+
+            Opacity = OpacityVar;
+        }
+
+        private void btnOpCsok_Click(object sender, EventArgs e)
+        {
+            if (OpacityVar < 1)
+            {
+                OpacityVar += 0.1f;
+            }
+
+            Opacity = OpacityVar;
+        }
+
+        private void btnFelul_Click(object sender, EventArgs e)
+        {
+            Location = new Point(Location.X, 0);
+        }
+
+        private void btnAlul_Click(object sender, EventArgs e)
+        {
+            windowsizeheight = Screen.GetWorkingArea(this).Height;
+            Location = new Point(Location.X, windowsizeheight - Height);
+        }
+
+        private void btnFel_Click(object sender, EventArgs e)
+        {
+            if(Location.Y>0)
+            Location = new Point(Location.X, Location.Y-5);
+            else
+            Location = new Point(Location.X, 0);
+
+        }
+    }
+}
