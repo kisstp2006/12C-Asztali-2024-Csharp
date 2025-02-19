f7d0d9ebc08b9f343378977dfaa8a95607dfff55
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-02-19 07:47:26 +0100

    U1

---
+++ UIApp/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace UIApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            btn_clickme = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // btn_clickme
+            // 
+            resources.ApplyResources(btn_clickme, "btn_clickme");
+            btn_clickme.Name = "btn_clickme";
+            btn_clickme.UseVisualStyleBackColor = true;
+            btn_clickme.Click += btn_clickme_Click;
+            btn_clickme.MouseEnter += btn_hover;
+            btn_clickme.MouseLeave += b;
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(checkBox1, "checkBox1");
+            checkBox1.Name = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(checkBox1);
+            Controls.Add(btn_clickme);
+            MinimizeBox = false;
+            Name = "MainForm";
+            Opacity = 0.95D;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_clickme;
+        private CheckBox checkBox1;
+    }
+}
---
+++ UIApp/Form1.cs
@@ -0,0 +1,80 @@
+namespace UIApp
+{
+    public partial class MainForm : Form
+    {
+        private bool felfele=true, iranybalra = true;
+        private int lepesfel = 20, lepesoldal=20;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_clickme_Click(object sender, EventArgs e)
+        {
+            btn_clickme.Text = "Megnyomtál";
+            Point p = btn_clickme.Location;
+            Console.WriteLine(p);
+            if (p.X- lepesoldal > 0 && iranybalra && p.Y - lepesfel > 0 && felfele)
+            {
+                btn_clickme.Location = new Point(btn_clickme.Location.X-20 , btn_clickme.Location.Y - 20);
+
+            }
+            if (p.X + lepesoldal < Width && iranybalra && p.Y + lepesfel < Height && felfele)
+            {
+                btn_clickme.Location = new Point(btn_clickme.Location.X - 20, btn_clickme.Location.Y - 20);
+
+            }
+
+            else
+            {
+                if (!iranybalra)
+                {
+                    btn_clickme.Location = new Point(btn_clickme.Location.X-0 , btn_clickme.Location.Y + 20);
+
+                }
+                if (!felfele)
+                {
+                    btn_clickme.Location = new Point(btn_clickme.Location.X + 20, btn_clickme.Location.Y -0);
+
+                }
+            }
+
+                if (p.X - lepesoldal < 0)
+                {
+                    felfele = false;
+                     iranybalra = false;
+
+            }
+            if (p.Y - lepesfel < 0)
+                {
+                    iranybalra = false;
+                    felfele = false;
+
+
+            }
+
+        }
+
+        private void btn_hover(object sender, EventArgs e)
+        {
+            btn_clickme.BackColor = Color.Black;
+            btn_clickme.ForeColor = Color.White;
+            btn_clickme.Width = 470;
+
+
+        }
+
+        private void b(object sender, EventArgs e)
+        {
+            btn_clickme.BackColor = Color.White;
+            btn_clickme.ForeColor = Color.Black;
+            btn_clickme.Width = 370;
+
+        }
+    }
+}
