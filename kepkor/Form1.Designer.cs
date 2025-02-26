f38564e5f001988e0e7a6cb62b4ca4489ead432a
Tibor Péter Kiss <2021133@dszcberegszaszi.hu>
2025-02-26 10:22:47 +0100

    Add new projects and resources to solution
    Updated `12C-Csharp.sln` to include "Kepekkorbe" and "kepkor" projects with their configurations. Modified `App.xaml` in "WPFMozog" to add new resource dictionaries.
    
    For "Kepekkorbe" project:
    - Added `App.xaml` and `App.xaml.cs` for application setup.
    - Added `AssemblyInfo.cs` for theme information.
    - Created `Kepekkorbe.csproj` targeting .NET 8.0 for Windows with WPF enabled.
    - Added `MainWindow.xaml` and `MainWindow.xaml.cs` for main window layout and code-behind.
    
    For "kepkor" project:
    - Added `kepkor.csproj` targeting .NET 8.0 for Windows Forms.
    - Added `Program.cs` defining the main entry point.
    - Added `Form1.cs` and `Form1.Designer.cs` for form layout and functionality.
    
    Added new PNG image files: `fej2.png`, `fej8.png`, `forgbal.png`, and `forgjobb.png`.

---
+++ kepkor/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace kepkor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Image = (Image)resources.GetObject("label1.Image");
+            label1.Location = new Point(355, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 82);
+            label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.Image = (Image)resources.GetObject("label2.Image");
+            label2.Location = new Point(512, 133);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 82);
+            label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.Image = (Image)resources.GetObject("label3.Image");
+            label3.Location = new Point(486, 335);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 82);
+            label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.Image = (Image)resources.GetObject("label4.Image");
+            label4.Location = new Point(101, 202);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 82);
+            label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.Image = (Image)resources.GetObject("label5.Image");
+            label5.Location = new Point(203, 86);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 82);
+            label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            label6.Image = (Image)resources.GetObject("label6.Image");
+            label6.Location = new Point(43, 36);
+            label6.Name = "label6";
+            label6.Size = new Size(100, 82);
+            label6.TabIndex = 5;
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.Image = (Image)resources.GetObject("label7.Image");
+            label7.Location = new Point(544, 37);
+            label7.Name = "label7";
+            label7.Size = new Size(100, 82);
+            label7.TabIndex = 6;
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.Image = (Image)resources.GetObject("label8.Image");
+            label8.Location = new Point(342, 359);
+            label8.Name = "label8";
+            label8.Size = new Size(100, 82);
+            label8.TabIndex = 7;
+            // 
+            // label9
+            // 
+            label9.Image = (Image)resources.GetObject("label9.Image");
+            label9.Location = new Point(203, 335);
+            label9.Name = "label9";
+            label9.Size = new Size(100, 82);
+            label9.TabIndex = 8;
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.Image = (Image)resources.GetObject("label10.Image");
+            label10.Location = new Point(544, 232);
+            label10.Name = "label10";
+            label10.Size = new Size(100, 82);
+            label10.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+    }
+}
---
+++ kepkor/Form1.cs
@@ -0,0 +1,75 @@
+namespace kepkor
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Forgats();
+
+        }
+        public async Task Forgats()
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                ChangeImg();
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ChangeImg()
+        {
+            var tempImage = label10.Image;
+
+
+            label10.Image = label9.Image;
+            label9.Image = label8.Image;
+            label8.Image = label5.Image;
+            label5.Image = label4.Image;
+            label4.Image = label3.Image;
+            label3.Image = label2.Image;
+            label2.Image = label1.Image;
+
+
+            label1.Image = tempImage;
+
+        }
+        private void ChangeImgBackward()
+        {
+            var tempImage = label1.Image;
+
+
+            label9.Image = label10.Image;
+            label8.Image = label9.Image;
+            label5.Image = label8.Image;
+            label4.Image = label5.Image;
+            label3.Image = label4.Image;
+            label2.Image = label3.Image;
+            label1.Image = label2.Image;
+
+
+            label10.Image = tempImage;
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            ChangeImg();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            ChangeImg();
+        }
+    }
+}
